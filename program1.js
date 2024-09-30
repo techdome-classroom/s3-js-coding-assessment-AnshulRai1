868f9fd19a0e0512d7a3fc6c1464e32c187866b5
Anshul <anshurai2503@gmail.com>
2024-09-30 15:07:39 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,26 +3,33 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-     let stack = [];
+    function isValid(s) {
+        // Stack to keep track of opening brackets
+        let stack = [];
+        
+        // Mapping of closing to opening brackets
+        const map = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        };
     
-     const map = {
-         ')': '(',
-         '}': '{',
-         ']': '['
-     };
- 
-     for (let char of s) {
-         if (char === '(' || char === '{' || char === '[') {
-             stack.push(char);
-         } else if (char === ')' || char === '}' || char === ']') {
-             
-             if (stack.length === 0 || stack.pop() !== map[char]) {
-                 return false;
-             }
-         }
-     }
-     
-     return stack.length === 0; 
+        // Loop through each character in the string
+        for (let char of s) {
+            if (char === '(' || char === '{' || char === '[') {
+                // Push opening brackets onto the stack
+                stack.push(char);
+            } else if (char === ')' || char === '}' || char === ']') {
+                // Check if the stack is empty or top of the stack matches the corresponding opening bracket
+                if (stack.length === 0 || stack.pop() !== map[char]) {
+                    return false;
+                }
+            }
+        }
+        
+        // If the stack is empty, all opening brackets had matching closing brackets
+        return stack.length === 0;
+    }
 };
 
 module.exports = { isValid };
