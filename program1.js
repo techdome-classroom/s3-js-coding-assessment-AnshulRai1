e276215e76731b1d179067ca273d1f88f262d8fb
Anshul <anshurai2503@gmail.com>
2024-09-30 15:06:30 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,7 +3,26 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+     let stack = [];
     
+     const map = {
+         ')': '(',
+         '}': '{',
+         ']': '['
+     };
+ 
+     for (let char of s) {
+         if (char === '(' || char === '{' || char === '[') {
+             stack.push(char);
+         } else if (char === ')' || char === '}' || char === ']') {
+             
+             if (stack.length === 0 || stack.pop() !== map[char]) {
+                 return false;
+             }
+         }
+     }
+     
+     return stack.length === 0; 
 };
 
 module.exports = { isValid };
