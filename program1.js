eb8cd7a8dd7220c7972e0b71a3e5035ce02e9613
Anshul <anshurai2503@gmail.com>
2024-09-30 15:08:27 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -16,7 +16,7 @@ var isValid = function(s) {
                
                 stack.push(char);
             } else if (char === ')' || char === '}' || char === ']') {
-                // Check if the stack is empty or top of the stack matches the corresponding opening bracket
+                
                 if (stack.length === 0 || stack.pop() !== map[char]) {
                     return false;
                 }
