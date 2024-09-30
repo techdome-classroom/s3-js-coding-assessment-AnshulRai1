0c69eb40aef3971092a2a43cc364feed21919b33
Anshul <anshurai2503@gmail.com>
2024-09-30 15:09:41 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,7 +3,36 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
     
+    let total = 0;
+    
+    // Loop through each character of the string
+    for (let i = 0; i < s.length; i++) {
+        // Get the value of the current Roman numeral
+        const currentVal = romanMap[s[i]];
+        // Get the value of the next Roman numeral (if it exists)
+        const nextVal = romanMap[s[i + 1]];
+
+        // If the current value is smaller than the next value, subtract it
+        if (nextVal && currentVal < nextVal) {
+            total -= currentVal;
+        } else {
+            // Otherwise, add the current value
+            total += currentVal;
+        }
+    }
+    
+    return total;
+
 };
 
 
